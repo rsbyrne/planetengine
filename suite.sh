abcafb91c98c4a679475ad1b4797497bfeeba058
Rohan Byrne <rsbyrne@6250d-144245-l.mobility.unimelb.net.au>
2019-01-29 14:38:16 +1100

    Cleaned up repo (got rid of .pyc files, etc.)

--- suite.sh
+++ suite.sh
@@ -5,7 +5,7 @@ i=0
 end=100
 DIR="$(cd "$(dirname "$0")" && pwd)"
 while [ $i -le $end ]; do
-    sudo sh $DIR/uw_ann_run.sh $SCRIPT $CORES $i &> /dev/null &
+    sudo sh $DIR/uw_ann_run.sh $SCRIPT $CORES $i > /dev/null 2> /dev/null &
     echo $i
     i=$(($i+1))
 done
