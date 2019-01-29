9b9bed89b5fa48952854dba71f99aa147a373896
Rohan Byrne <rsbyrne@6250d-144245-l.mobility.unimelb.net.au>
2019-01-29 14:41:01 +1100

    More cleanup.

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-i=0
-end=5
-while [ $i -le $end ]; do
-    echo $i
-    i=$(($i+1))
-done
-
--- test2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if sh test.sh
-then
-echo 'hello world'
-else
-echo 'no joy'
-fi
