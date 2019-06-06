b85482bdeb8b8910b611e06964e267fffa56056c
Rohan S Byrne <rohan.byrne@gmail.com>
2019-06-06 11:48:10 +1000

    Fixed shell line endings.

--- linux/suite.sh
+++ linux/suite.sh
@@ -5,7 +5,8 @@ CORESPERCHUNK=${3:-1}
 SCRIPT=${1:-localscript.py}
 i=0
 end=$CHUNKS
-while [ $i -le $end ]; do
+while [ $i -le $end ]
+do
     touch job$i.out
     touch job$i.error
     mpirun -np $CORESPERCHUNK python $SCRIPT $CHUNKS $i > job$i.out 2> job$i.error &
