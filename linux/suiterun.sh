a9029184228a201d2f7ddb84f35c0709f58611d3
rsbyrne <rsbyrne@6250d-144245-l.mobility.unimelb.net.au>
2019-06-11 16:52:48 +1000

    Another day of work.

--- linux/suiterun.sh
+++ linux/suiterun.sh
@@ -10,7 +10,9 @@ while [ $CHUNKNO -lt $CHUNKS ]
 do
     touch logs/job$CHUNKNO.out
     touch logs/job$CHUNKNO.error
-    mpirun -np $CORESPERCHUNK python $SCRIPT $CHUNKS $CHUNKNO > logs/job$CHUNKNO.out 2> logs/job$CHUNKNO.error &
+    sudo docker run -v $PWD:/workspace/user_data -it underworldcode/uw2cylindrical:cylindrical \
+        mpirun -np $CORESPERCHUNK python /workspace/user_data/$SCRIPT $CHUNKS $CHUNKNO \
+        > logs/job$CHUNKNO.out 2> logs/job$CHUNKNO.error &
     echo "Submitted chunk " $CHUNKNO
     CHUNKNO=$(($CHUNKNO+1))
 done
