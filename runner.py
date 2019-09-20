461f48387368571e61312a58979007d7d5e0b039
Rohan S Byrne <rohan.byrne@gmail.com>
2019-09-20 14:08:59 +1000

    Incremental progress on runner.

--- runner.py
+++ runner.py
@@ -1,21 +1,43 @@
 import sys
+import os
 
 script = str(sys.argv[1])
 chunks = int(sys.argv[2])
-jobs
 coresperchunk = int(sys.argv[4])
+
+from .utilities import hashstamp
+from . import mpi
+from .paths import workPath
+from . import disk
+
+import time
+presenttime = None
+if mpi.rank == 0:
+    presenttime = time.time()
+presenttime = mpi.comm.bcast(presenttime, root = 0)
+
+campaignName = hashstamp([script, presenttime])
+campaignLog = []
+disk.save_json(campaignName, workPath)
 
 import subprocess
 
-while
-subprocess.call(['chmod', '-R', '777', workPath])
-subprocess.call([
-    'mpirun',
-    '-np',
-    str(coresperchunk),
-    'python',
-    str(script),
-    str(chunks),
-    str(chunkno),
-    str(iterno)
-    ])
+for chunk in chunks:
+    iterno = 0
+    while True:
+        campaignLog = disk.load_json(campaignName, workPath)
+        if 
+        with open(campaignFile, 'w') as file:
+            file.write(campaignName)
+        subprocess.call([
+            'mpirun',
+            '-np',
+            str(coresperchunk),
+            'python',
+            str(script),
+            str(chunks),
+            str(chunkno),
+            str(iterno),
+            campaignName
+            ])
+        iterno += 1
