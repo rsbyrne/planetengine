a870e13fd9dc953c4922c51cdaa955d9db9da34e
Rohan S Byrne <rohan.byrne@gmail.com>
2019-09-21 11:20:19 +1000

    Improvements to suite running.

--- linux/chunkrun.sh
+++ linux/chunkrun.sh
@@ -3,13 +3,13 @@ umask 0000
 SCRIPT=${1:-localscript.py}
 CHUNKS=${2:-1}
 CHUNKNO=${3:-0}
-CORES=${4:-1}
+JOBSPERCHUNK=${4:-1}
+CORES=${5:-1}
 ITERNO=0
-MAXITER=999
+MAXITER=$JOBSPERCHUNK
+SCRIPTSDIR=/home/jovyan/workspace/planetengine/linux
 while [ $ITERNO -lt $MAXITER ]
 do
-    # echo "Started job " $CHUNKNO $ITERNO
-    mpirun -np $CORES python $SCRIPT $CHUNKS $CHUNKNO $ITERNO > logs/job$CHUNKNO.out 2> logs/job$CHUNKNO.error
-    # echo "Finished job " $CHUNKNO $ITERNO
+    $SCRIPTSDIR/singlerun.sh $SCRIPT $CHUNKS $CHUNKNO $ITERNO $CORES
     ITERNO=$(($ITERNO+1))
 done
--- linux/suiterun.sh
+++ linux/suiterun.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 umask 0000
 SCRIPT=${1:-localscript.py}
-CHUNKS=${2:-1}
-CORESPERCHUNK=${3:-1}
+JOBS=${2:-1}
+CHUNKS=${3:-1}
+CORESPERCHUNK=${4:-1}
 CHUNKNO=0
+JOBSPERCHUNK=$((($JOBS+$CHUNKS-1)/$CHUNKS))
+echo $JOBSPERCHUNK
 rm -rf logs
 mkdir -p logs
 # SCRIPTSDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -12,7 +15,7 @@ while [ $CHUNKNO -lt $CHUNKS ]
 do
     touch logs/job$CHUNKNO.out
     touch logs/job$CHUNKNO.error
-    sh $SCRIPTSDIR/chunkrun.sh $SCRIPT $CHUNKS $CHUNKNO $CORESPERCHUNK &
+    $SCRIPTSDIR/chunkrun.sh $SCRIPT $CHUNKS $CHUNKNO $JOBSPERCHUNK $CORESPERCHUNK &
     echo "Submitted chunk " $CHUNKNO
     CHUNKNO=$(($CHUNKNO+1))
 done
