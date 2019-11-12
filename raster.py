fa292a4e354bdbcfa5af91d4e581935c8a4f084d
Rohan Byrne <rsbyrne@6250d-144245-l.mobility.unimelb.net.au>
2019-11-12 15:46:34 +1100

    Added 'save' feature to rasters.

--- raster.py
+++ raster.py
@@ -1,5 +1,6 @@
 import underworld as uw
 import numpy as np
+import os
 from . import mpi
 from . import fieldops
 from .utilities import message
@@ -7,15 +8,15 @@ from . import functions as pfn
 from PIL import Image
 
 class Data:
-    def __init__(self, inVar, size = 16):
+    def __init__(self, inVar, size = (16, 16)):
         mesh = uw.mesh.FeMesh_Cartesian(
-            elementRes = (size, size)
+            elementRes = size
             )
         self.var = mesh.add_variable(1)
         self.var.scales = [[-128., 127.]]
         self.inVar = pfn.projection.get_meshVar(inVar)
         self.size = size
-        self.data = np.zeros((size, size)).astype('int8')
+        self.data = np.zeros(size).astype('int8')
         self.update()
     def update(self):
         tolerance = fieldops.copyField(
@@ -25,35 +26,29 @@ class Data:
         data = None
         if mpi.rank == 0:
             data = self.var.evaluate_global(self.var.mesh.subMesh.data)
-            data = data.reshape([self.size, self.size])
+            data = data.reshape(self.size)
             data = np.flip(data, axis = 0)
             data = data.astype('int8')
         data = mpi.comm.bcast(data, root = 0)
         self.data[...] = data
 
 class Raster:
-    def __init__(self, *args, size = 16):
-        if len(args) == 1:
-            dim = 1
-            mode = 'L'
-        elif len(args) == 2:
-            dim = 2
-            mode = 'RGB'
-        elif len(args) == 3:
-            dim = 3
-            mode = 'RGB'
-        else:
-            raise Exception
+    '''
+    Modes: 1, L, P, RGB, RGBA, CMYK, YCbCr, LAB, HSV, I, F, RGBa, LA, RGBX
+    '''
+    def __init__(self, *args, size = (16, 16), mode = 'L', name = 'anon', add = None):
+        self.name = name
+        self.mode = mode
+        self.add = add
         self.dataObjs = [Data(arg, size = size) for arg in args]
-        self.dataArrays = [dataObj.data for dataObj in self.dataObjs]
-        if dim == 2:
-            zerodata = np.zeros((size, size)).astype('int8')
-            self.dataArrays.append(zerodata)
-        self.img = Image.new(
-            mode,
-            (size, size),
-            "black"
-            )
+        self.bands = []
+        for obj in self.dataObjs:
+            newimg = Image.new(
+                'L',
+                size,
+                "black"
+                )
+            self.bands.append(newimg)
         self.update()
     def update(self):
         self._update_data()
@@ -62,10 +57,32 @@ class Raster:
         for dataObj in self.dataObjs:
             dataObj.update()
     def _update_img(self):
-        pixels = self.img.load()
-        for i in range(self.img.size[0]):
-            for j in range(self.img.size[1]):
-                pixels[i, j] = tuple([
-                    dataArray[j, i] + 128 \
-                        for dataArray in self.dataArrays
-                    ])
+        for band, dataObj in zip(self.bands, self.dataObjs):
+            pixels = band.load()
+            dataArray = dataObj.data
+            for i in range(band.size[0]):
+                for j in range(band.size[1]):
+                    pixels[i, j] = (dataArray[j, i] + 128,)
+        self.img = Image.merge(self.mode, self.bands)
+    def save(self, path = '', name = None, add = None, ext = 'png'):
+        self.update()
+        if name is None:
+            name = self.name
+        if add is None:
+            if not self.add is None:
+                add = self.add
+            else:
+                add = ''
+        if callable(add):
+            add = add()
+        if type(add) == int:
+            add = '_' + str(add).zfill(8)
+        elif len(add) > 0:
+            add = '_' + str(add)
+        name += add
+        if mpi.rank == 0:
+            if not os.path.isdir(path):
+                os.makedirs(path)
+            assert os.path.isdir(path)
+        # mpi.barrier()
+        self.img.save(os.path.join(path, name + '.' + ext))
