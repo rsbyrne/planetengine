faaa1ea3d672fa38a62887905fc3930b135d6661
Rohan Byrne <rsbyrne@6250d-144245-l.mobility.unimelb.net.au>
2019-11-27 11:13:58 +1100

    More cleaning.

--- utilities.py
+++ utilities.py
@@ -14,40 +14,6 @@ def message(*args):
         if mpi.rank == 0:
             print(arg)
 
-# import random
-# import itertools
-
-# def suite_list(listDict, shuffle = False, chunks = None, shuffleseed = 1066):
-#     listOfKeys = sorted(listDict)
-#     listOfVals = []
-#     listOfDicts = []
-#     for key in listOfKeys:
-#         val = listDict[key]
-#         if type(val) == list:
-#             entry = val
-#         else:
-#             entry = [val]
-#         listOfVals.append(entry)
-#     combinations = list(itertools.product(*listOfVals))
-#     for item in combinations:
-#         newDict = {key: val for key, val in zip(listOfKeys, item)}
-#         listOfDicts.append(newDict)
-#     if shuffle:
-#         random.seed(shuffleseed)
-#         random.shuffle(listOfDicts)
-#         random.seed()
-#     if chunks is None or chunks == 0:
-#         outList = listOfDicts
-#     else:
-#         outList = split_list(listOfDicts, chunks)
-#     return outList
-#
-# def split_list(a, n):
-#     k, m = divmod(len(a), n)
-#     return list(
-#         (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
-#         )
-
 def get_substrates(var):
     if type(var) == uw.mesh._meshvariable.MeshVariable:
         meshes = [var.mesh,]
