dc01d894d6d9df3fdee5b6e86cc0d131e1a43436
Rohan S Byrne <rohan.byrne@gmail.com>
2019-11-29 00:09:01 +1100

    Tested - works in parallel!

--- tests/rasterpara.py
+++ tests/rasterpara.py
@@ -1,15 +1,31 @@
 import planetengine
-system = planetengine.systems.isovisc.build(res = 16)
-observer = planetengine.observers.standard.build(system)
-observer.store()
-if planetengine.mpi.rank == 0:
-    print(observer.stored)
-# raster = planetengine.visualisation.Raster(
-#     system.varsOfState['temperature'],
-#     system.varsOfState['temperature'],
-#     system.varsOfState['temperature'],
-#     )
-# raster.update()
+
+system = planetengine.systems.isovisc.build(
+    res = 16,
+    f = 0.5,
+    aspect = 2.,
+    _initial_temperature = planetengine.initials.sinusoidal.build(freq = 2)
+    )
+var1 = system.obsVars['temperature']
+var2 = planetengine.functions.component.rad(system.obsVars['velocity'])
+var3 = planetengine.functions.component.ang(system.obsVars['velocity'])
+
+myraster = planetengine.visualisation.Raster(var1, var2, var3)
+
+myraster.img.save('test.png')
+
+# import planetengine
+# system = planetengine.systems.isovisc.build(res = 16)
+# observer = planetengine.observers.standard.build(system)
+# observer.store()
 # if planetengine.mpi.rank == 0:
-#     print(raster.data)
-#     print(raster.data.shape)
+#     print(observer.stored)
+# # raster = planetengine.visualisation.Raster(
+# #     system.varsOfState['temperature'],
+# #     system.varsOfState['temperature'],
+# #     system.varsOfState['temperature'],
+# #     )
+# # raster.update()
+# # if planetengine.mpi.rank == 0:
+# #     print(raster.data)
+# #     print(raster.data.shape)
