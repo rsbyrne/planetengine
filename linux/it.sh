e5e375278c37c4e9f4f2b56ba3ca020e28207e38
Rohan S Byrne <rohan.byrne@gmail.com>
2019-12-05 19:32:08 +1100

    Changed, then unchanged, an update to use a different docker image.

--- linux/it.sh
+++ linux/it.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 WORKDIR='/home/jovyan/workspace/'
 IMAGE='underworldcode/uw2cylindrical:cylindrical'
+# IMAGE='rsbyrne/phd:latest'
 docker run -v $PWD:$WORKDIR -it $IMAGE bash
--- linux/nb.sh
+++ linux/nb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 WORKDIR='/home/jovyan/workspace/'
 IMAGE='underworldcode/uw2cylindrical:cylindrical'
+# IMAGE='rsbyrne/phd:latest'
 docker run -v $PWD:$WORKDIR -p 8888:8888 $IMAGE
