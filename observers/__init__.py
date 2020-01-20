c1781b3ce72b22577855c08c7b7663b371c09742
Rohan S Byrne <rohan.byrne@gmail.com>
2020-01-21 08:13:53 +1100

    Minor tweaks.

--- observers/__init__.py
+++ observers/__init__.py
@@ -7,52 +7,27 @@ class Observer(Built):
     def __init__(
             self,
             inputs,
-            script
+            script,
+            system,
+            outDict
             ):
+
+        outDict['modeltime'] = system.modeltime
+
+        system.attach_observer(self)
+
+        self.system = system
+        self.outDict = outDict
+        self.outkeys = [*sorted(self.outDict)]
+
         super().__init__(
             inputs = inputs,
             script = script
             )
 
-# class Observer(everest.built.Built):
-#
-#     type = 'observer'
-#
-#     def __init__(
-#             self,
-#             inputs,
-#             script,
-#             system,
-#             outDict
-#             ):
-#
-#         outDict['modeltime'] = system.modeltime
-#
-#         system.attach_observer(self)
-#
-#         self.orders = lambda: False
-#         self.system = system
-#         self.outDict = outDict
-#         self.outkeys = [*sorted(self.outDict)]
-#
-#         super().__init__(
-#             inputs = inputs,
-#             script = script
-#             )
-#
-#     def update(self):
-#         self.count.value = self.system.count()
-#
-#     def out(self):
-#         outs = []
-#         for key in self.outkeys:
-#             outs.append(self.outDict[key].evaluate())
-#         return outs
-#
-#     def set_orders(self, orders):
-#         self.orders = orders
-#
-#     # def prompt(self):
-#     #     if self.orders():
-#     #         self.store()
-#     #         if len(self.stored) >
+    def out(self):
+        self.count.value = self.system.count()
+        outs = []
+        for key in self.outkeys:
+            outs.append(self.outDict[key].evaluate())
+        return outs
