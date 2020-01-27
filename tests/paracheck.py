d19841bc0656aaf14da1100fe43797415b29ed3f
Rohan S Byrne <rohan.byrne@gmail.com>
2020-01-27 19:43:04 +1100

    Completely broken!

--- tests/paracheck.py
+++ tests/paracheck.py
@@ -1,5 +1,8 @@
+import time
+
 import planetengine
 import everest
+from everest.mpi import message
 
 from planetengine.systems import isovisc
 
@@ -22,9 +25,13 @@ system.anchor('test', '.')
 system.save()
 
 system_identical = isovisc.build(Ra = 1e4, res = 16, f = 0.9)
+# assert system_identical.constructor is system.constructor
+assert system_identical.configs['temperatureField'] is system.configs['temperatureField']
 assert system_identical is system
 
 system2 = isovisc.build(Ra = 1e4, res = 16, f = 0.8)
+# assert system2.constructor is system.constructor
+assert system2.configs['temperatureField'] is system.configs['temperatureField']
 assert not system2 is system
 system.clear()
 system.store()
@@ -33,7 +40,12 @@ system.load(1)
 system.store()
 system.save()
 
+# if mpi.rank == 0:
+#     myfile = system.file()
+#     assert system.constructor.hashID in myfile.keys()
+
 from everest.builts import load
+
 system_got = load(system.hashID, 'test', '.')
 assert system_got is system
 
