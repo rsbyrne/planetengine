9c14b24ed434e777366f105e23a772bb1fb1b3d0
Rohan S Byrne <rohan.byrne@gmail.com>
2020-01-28 11:35:11 +1100

    Passed tests!

--- tests/paracheck.py
+++ tests/paracheck.py
@@ -1,18 +1,10 @@
-import time
-
-import planetengine
-import everest
-from everest import disk
-from everest import builts
-from everest.mpi import message
-
-from planetengine.systems import isovisc
-
 import os
 from everest import mpi
 if mpi.rank == 0:
     if os.path.exists('test.frm'):
         os.remove('test.frm')
+
+from planetengine.systems import isovisc
 
 system = isovisc.build(Ra = 1e4, res = 16, f = 0.9)
 system.anchor('test', '.')
@@ -29,12 +21,10 @@ system.iterate()
 system.save()
 
 system_identical = isovisc.build(Ra = 1e4, res = 16, f = 0.9)
-# assert system_identical.constructor is system.constructor
 assert system_identical.configs['temperatureField'] is system.configs['temperatureField']
 assert system_identical is system
 
 system2 = isovisc.build(Ra = 1e4, res = 16, f = 0.8)
-# assert system2.constructor is system.constructor
 assert system2.configs['temperatureField'] is system.configs['temperatureField']
 assert not system2 is system
 system.clear()
@@ -45,15 +35,16 @@ system.store()
 system.save()
 
 from everest.builts import load
+from everest.builts import _PREBUILTS
 
 got_built = load(system.configs['temperatureField'].hashID, 'test', '.')
 assert got_built is system.configs['temperatureField']
 
-system_got = everest.builts._PREBUILTS[system.hashID]()
+system_got = _PREBUILTS[system.hashID]()
 system_got = load(system.hashID, 'test', '.')
 assert system_got is system
 
-del everest.builts._PREBUILTS[system.hashID]
+del _PREBUILTS[system.hashID]
 
 system_loaded = load(system.hashID, 'test', '.')
 assert not system_loaded is system
