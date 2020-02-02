88c1647d353b144b74032262c8931af225a89600
Rohan S Byrne <rohan.byrne@gmail.com>
2020-02-03 10:43:44 +1100

    Fixed paracheck1

--- tests/paracheck1.py
+++ tests/paracheck1.py
@@ -12,17 +12,30 @@ from planetengine.initials.constant import build as constant
 from planetengine.initials.load import build as load
 from planetengine.states.threshold import build as threshold
 
-task = isovisc(res = 32) \
-    [params(Ra = 1e5)] \
-    [configs(temperatureField = sinusoidal(), temperatureDotField = constant())] \
-    [threshold(val = 10)]
-# task.anchor('test', '..')
+system1 = isovisc(res = 32)
+params1 = params(Ra = 1e5)
+tempIC1 = sinusoidal()
+tempDotIC1 = constant()
+configs1 = configs(temperatureField = tempIC1, temperatureDotField = tempDotIC1)
+threshold1 = threshold(val = 10)
+case1 = system1[params1]
+real1 = case1[configs1]
+task1 = real1[threshold1]
 
-task()
+system2 = isovisc(res = 64)
+params2 = params(Ra = 1e6)
+tempIC2 = load(real = task1.arg, varName = 'temperatureField')
+tempDotIC2 = load(real = task1.arg, varName = 'temperatureDotField')
+configs2 = configs(temperatureField = tempIC2, temperatureDotField = tempDotIC2)
+threshold2 = threshold(val = 20)
+case2 = system2[params1]
+real2 = case2[configs2]
+task2 = real2[threshold2]
 
-task2 = isovisc(res = 64)\
-    [params(Ra = 1e5)] \
-    [configs(temperatureField = load(real = task.arg, varName = 'temperatureField'), temperatureDotField = constant())] \
-    [threshold(val = 10)]
-# task2.anchor('test', '..')
+task2.anchor('test', '.')
+
+task2()
+
+mpi.message(sorted(task2.reader['*'].keys()))
+
 mpi.message("Complete!")
