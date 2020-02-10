8cc39a9262f2168753d7fbbd9cfe0012d574423a
Rohan Byrne <rsbyrne@6250d-144245-l.mobility.unimelb.net.au>
2020-02-10 16:40:46 +1100

    Changed how dummy modules work again

--- campaign.py
+++ campaign.py
@@ -1,3 +1,81 @@
-from planetengine._campaign import Campaign
-CLASS = Campaign
-build, get = CLASS.build, CLASS.get
+import sys
+
+from everest.builts.container import Container
+from everest.builts._task import Task
+from everest.vectorset import VectorSet
+
+from .traverse import Traverse
+
+class CampaignIterable:
+    def __init__(self, system, state, observers, **vectorSets):
+        self.vectorSets = vectorSets
+        self.system, self.state, self.observers = system, state, observers
+    def __iter__(self):
+        self.vectors = iter(VectorSet(**self.vectorSets))
+        return self
+    def __next__(self):
+        vector = next(self.vectors)
+        out = Traverse(
+            self.system,
+            self.state,
+            self.observers,
+            express = True,
+            **vector
+            )
+        return out
+
+class Campaign(Container, Task):
+
+    script = '''from planetengine.campaign import Campaign as CLASS'''
+
+    def __init__(self,
+            system = None,
+            state = None,
+            observers = [],
+            cores = 1,
+            **vectorSets
+            ):
+
+        self.cores = cores
+
+        iterable = CampaignIterable(
+            system,
+            state,
+            observers,
+            **vectorSets
+            )
+
+        self._campaign_halt_toggle = False
+        self._held_ticket = None
+
+        super().__init__(iterable = iterable)
+
+        # Task attributes:
+        self._task_initialise_fns.append(self.initialise)
+        self._task_cycler_fns.append(self._campaign_cycle)
+        self._task_stop_fns.append(self._campaign_halt)
+        # self._task_finalise_fns.append()
+
+    def _campaign_cycle(self):
+        try:
+            for ticket in self:
+                self._held_ticket = ticket
+                traverse = ticket()
+                traverse.subrun(self.hashID, self.cores)
+                self.complete(ticket)
+                self._held_ticket = None
+            else:
+                self._campaign_halt_toggle = True
+        except:
+            self.checkBack(self._held_ticket)
+            self._held_ticket = None
+            exc_type, exc_val = sys.exc_info()[:2]
+            output = exc_type(exc_val)
+            raise output
+
+    def _campaign_halt(self):
+        if self._campaign_halt_toggle:
+            self._campaign_halt_toggle = False
+            return True
+        else:
+            return False
--- traverse.py
+++ traverse.py
@@ -1,3 +1,91 @@
-from planetengine._traverse import Traverse
-CLASS = Traverse
-build, get = CLASS.build, CLASS.get
+import time
+
+from everest.builts._task import Task
+from everest.builts.states import State
+from everest.builts.states.threshold import Threshold
+from everest.builts._iterator import LoadFail
+from everest.builts._counter import Counter
+from everest.builts import check_global_anchor
+from everest.builts import _get_info
+
+from everest import mpi
+
+class Traverse(Counter, Task):
+
+    script = '''from planetengine.traverse import Traverse as CLASS'''
+
+    @staticmethod
+    def _process_inputs(inputs):
+        state = inputs['state']
+        if not isinstance(state, State):
+            if type(state) is int: prop = 'count'
+            elif type(state) is float: prop = 'chron'
+            else: raise TypeError
+            inputs['state'] = Threshold(
+                prop = prop,
+                op = 'ge',
+                val = state
+                )
+
+    def __init__(self,
+            cosmos = None,
+            state = None,
+            express = True,
+            observers = [],
+            **vector
+            ):
+
+        check_global_anchor()
+
+        self.cosmos, self.state, self.express, self.observers, self.vector = \
+            cosmos, state, express, observers, vector
+
+        ignoreme1, self.vectorHash, ignoreme2, self.systemHashID = \
+            _get_info(cosmos, vector)
+
+        self.localObjects['traversee'] = self.systemHashID
+        self.localObjects['vector'] = self.vectorHash
+
+        super().__init__(_iterator_initialise = False)
+
+        # Task attributes:
+        self._task_initialise_fns.append(self._traverse_initialise)
+        self._task_cycler_fns.append(self._traverse_iterate)
+        self._task_stop_fns.append(self._traverse_stop)
+        self._task_finalise_fns.append(self._traverse_finalise)
+
+    def _traverse_initialise(self):
+        self.count.value = 0
+        self.traversee = self.cosmos(**self.vector)
+        if self.express:
+            try:
+                self.traversee.load(self.state)
+            except LoadFail:
+                if self.counts:
+                    self.traversee.load(max(self.counts))
+        self.count.value = self.traversee.count()
+        self.traversee.store()
+        self.traversee.save()
+        self._last_checkpoint_time = mpi.share(time.time())
+        for observer in self.observers: self.add_promptee(observer)
+
+    def _traverse_iterate(self):
+        self.traversee()
+        self.count += 1
+        time_now = mpi.share(time.time())
+        time_since_last_checkpoint = time_now - self._last_checkpoint_time
+        if time_since_last_checkpoint > 300.:
+            self.traversee.store()
+            self.store()
+            self._last_checkpoint_time = time_now
+
+    def _traverse_stop(self):
+        return self.state(self.traversee)
+
+    def _traverse_finalise(self):
+        self.traversee.store()
+        self.traversee.save()
+        self.store()
+        self.save()
+        del self.traversee
+        for observer in observers: self.remove_promptee(observer)
