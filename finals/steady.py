b5426bfd213eed5e7156fcd48bf997aac4d90d24
Rohan Byrne <rsbyrne@6250d-144245-l.mobility.unimelb.net.au>
2020-03-17 18:25:14 +1100

    More work on steady state,

--- finals/steady.py
+++ finals/steady.py
@@ -1,11 +1,17 @@
-from . import Final
-from ..observers import Thermo
-
-class Steady(Final):
-
-    def __init__(self,
-            system,
-            **kwargs
-            ):
-        self.observer = Thermo(system, **kwargs)
-        super().__init__()
+# from . import Final
+# from ..observers import Thermo
+#
+# class Steady(Final):
+#
+#     def __init__(self,
+#             system,
+#             **kwargs
+#             ):
+#
+#         self.observer = Thermo(system, **kwargs)
+#
+#         super().__init__()
+#
+#     def _zone_fn(self):
+#
+#     def _get_Nus(self):
