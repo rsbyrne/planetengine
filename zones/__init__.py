aa0f4f36a023bbb02b59b327b5cff3b2a025eca5
Rohan Byrne <rsbyrne@6250d-144245-l.mobility.unimelb.net.au>
2020-03-17 17:25:32 +1100

    Got a start on implementing Zones (like states, but better)

---
+++ zones/__init__.py
@@ -0,0 +1,22 @@
+from everest.builts._boolean import Boolean
+
+class Zone(Boolean):
+
+    def __init__(self,
+            **kwargs
+            ):
+
+        super().__init__(_bool_meta_fn = self._zone_meta_fn, **kwargs)
+
+        self._pre_bool_fns.extend(self._pre_zone_fn)
+        self._bool_fns.extend(self._zone_fn)
+        self._post_bool_fns.extend(self._post_zone_fn)
+
+    # Expect to be overwritten:
+    def _pre_zone_fn(self):
+        pass
+    def _zone_fn(self):
+        pass
+    def _post_zone_fn(self):
+        pass
+    _zone_meta_fn = all
---
+++ zones/steadystate.py
@@ -0,0 +1,8 @@
+from . import Zone
+
+class SteadyState(Zone):
+
+    def __init__(self,
+            system
+            ):
+        super().__init__()
